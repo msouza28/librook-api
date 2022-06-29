cc194517bba42145d404ca21c485b7e3dc4bdd81
msouza28 <matheus.emiliano28@gmail.com>
2022-06-29 14:57:01 -0300

    anotações de mapeamento JPA

--- src/main/java/br/com/project/librook/domain/Categoria.java
+++ src/main/java/br/com/project/librook/domain/Categoria.java
@@ -4,16 +4,24 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class Categoria {
+@Entity
+public class Categoria implements Serializable{
+     private static final long serialVersionUID = 1L;
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
     private String nome;
     private String descricao;
     // uma categoria pode tem 0 ou 1* livros
+   @OneToMany(mappedBy = "categoria")
     private List<Livro> livros = new ArrayList<>();
 
 }
--- src/main/java/br/com/project/librook/domain/Livro.java
+++ src/main/java/br/com/project/librook/domain/Livro.java
@@ -4,15 +4,27 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+import java.io.Serializable;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class Livro {
+@Entity
+public class Livro implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
     private String titulo;
     private String nomeAutor;
     private String texto;
     // um livro precisa do id da categoria
+    //varios livros para uma categoria
+    @ManyToOne
+    @JoinColumn(name = "categoria_id")
     private Categoria categoria;
 
 }
