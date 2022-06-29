d1c5f1095a48d335cf782188630be628b30bcdbc
msouza28 <matheus.emiliano28@gmail.com>
2022-06-29 09:57:35 -0300

    first commit

---
+++ src/main/java/br/com/project/librook/LibrookApplication.java
@@ -0,0 +1,13 @@
+package br.com.project.librook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/project/librook/LibrookApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.project.librook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
