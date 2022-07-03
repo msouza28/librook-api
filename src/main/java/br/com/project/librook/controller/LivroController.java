6d2a66c898b58e23f2bc41a599ac4eabdab61de7
msouza28 <matheus.emiliano28@gmail.com>
2022-07-03 19:16:58 -0300

    inclusao do metodo PUT update Livro

--- src/main/java/br/com/project/librook/controller/LivroController.java
+++ src/main/java/br/com/project/librook/controller/LivroController.java
@@ -31,4 +31,16 @@ public class LivroController {
         List<LivroDto> listDto = list.stream().map(obj -> new LivroDto(obj)).collect(Collectors.toList());
         return ResponseEntity.ok().body(listDto);
     }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<Livro> update(@PathVariable Integer id, @RequestBody Livro obj){
+         Livro newObj = livroService.update(id, obj);
+         return ResponseEntity.ok().body(newObj);
+    }
+
+    @PatchMapping(value = "/{id}")
+    public ResponseEntity<Livro> updatePatch(@PathVariable Integer id, @RequestBody Livro obj){
+        Livro newObj = livroService.update(id, obj);
+        return ResponseEntity.ok().body(newObj);
+    }
 }
