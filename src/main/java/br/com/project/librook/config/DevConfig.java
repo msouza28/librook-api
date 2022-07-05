db926d7e17144489e8a0fea812d755bfeab780db
msouza28 <matheus.emiliano28@gmail.com>
2022-07-05 15:43:12 -0300

    aplication-test.properties

--- src/main/java/br/com/project/librook/config/DevConfig.java
+++ src/main/java/br/com/project/librook/config/DevConfig.java
@@ -5,8 +5,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("dev")
 public class DevConfig { // configuration chama o metodo que foi instanciado pela injeção de dependecia
                         // e popula o DB
     @Autowired
---
+++ src/main/java/br/com/project/librook/config/TestConfig.java
@@ -0,0 +1,20 @@
+package br.com.project.librook.config;
+
+import br.com.project.librook.service.DBService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+
+    @Autowired
+    private DBService dbService;
+
+    @Bean
+    public void instaciaBaseDeDados() {
+        this.dbService.instanciaBaseDeDados();
+    }
+}
